f8518269452dd6686d1bab0a0c1f9ff7fbf46c89
Vanessa Hoffmann <hoffmann.vanessa13@gmail.com>
2020-04-02 00:42:15 +0000

    testing signs and ladder

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+ 
+public class World : SceneController {
+ 
+    public Transform player;
+ 
+    // Use this for initialization
+    public override void Start () {
+        base.Start();
+ 
+        if (prevScene == "Mountain_Pass") {
+            player.position = new Vector2(0f, 3f);
+            Camera.main.transform.position = new Vector3(0f, 3f, -10f);
+        }
+    }
+     
+}
