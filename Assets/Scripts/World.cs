8f522b6f13d8a1d6afd3af6d93ca9ff0f2c0dae2
Vanessa Hoffmann <hoffmann.vanessa13@gmail.com>
2020-04-02 04:26:48 +0000

    finished linking the scenes in mt pass

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -8,16 +8,25 @@ public class World : SceneController {
     public override void Start () {
         base.Start();
  
-        if (prevScene == "Mountain_Pass") {
-            player.position = new Vector2(-31.15f, -25.8f);
+        if (prevScene == "Mountain_Pass" && currentScene == "Mountain_Pass_Pitfall_1") {
+            player.position = new Vector2(.17f, 15.37f);
             Camera.main.transform.position = new Vector3(-35.5f, 13.91f, -10f);
       
             
         }
-         if (prevScene == "Mountain_Pass_Pitfall_2") {
-                 player.position = new Vector2(-36.03f, 12.59f);
+        if (prevScene == "Mountain_Pass" && currentScene == "Mountain_Pass_Pitfall_2") {
+            player.position = new Vector2(-31.15f, -25.8f);
+            Camera.main.transform.position = new Vector3(-35.5f, 13.91f, -10f);    
+        }        
+        if (prevScene == "Mountain_Pass_Pitfall_1") {
+            player.position = new Vector2(-36.03f, -12.59f);
             Camera.main.transform.position = new Vector3(-31.5f, -25.91f, -10f);
         }
+         if (prevScene == "Mountain_Pass_Pitfall_2") {
+                 player.position = new Vector2(-52.37f, -15.2f);
+            Camera.main.transform.position = new Vector3(-31.5f, -25.91f, -10f);
+        }
+
     }
      
 }
