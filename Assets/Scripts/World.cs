bd403fe2f62c9a70f3edff33cbcc15a58ba4f6bc
Vanessa Hoffmann <hoffmann.vanessa13@gmail.com>
2020-04-02 01:44:35 +0000

    linked pitfall 2 to a ladder in the mountain pass scene

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -9,8 +9,14 @@ public class World : SceneController {
         base.Start();
  
         if (prevScene == "Mountain_Pass") {
-            player.position = new Vector2(0f, 3f);
-            Camera.main.transform.position = new Vector3(0f, 3f, -10f);
+            player.position = new Vector2(-31.15f, -25.8f);
+            Camera.main.transform.position = new Vector3(-35.5f, 13.91f, -10f);
+      
+            
+        }
+         if (prevScene == "Mountain_Pass_Pitfall_2") {
+                 player.position = new Vector2(-36.03f, 12.59f);
+            Camera.main.transform.position = new Vector3(-31.5f, -25.91f, -10f);
         }
     }
      
