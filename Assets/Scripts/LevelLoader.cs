f046b78606df126b2ebfd70b9e9a8a448626b6d8
Nadin K <nadkhal18@gmail.com>
2020-04-03 09:02:37 +0000

    adjusted main camera to follow alfie, added alfie 1st transform prefab, game controller, level loader, and pitfall scripts and stuff to forest scene

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -43,6 +43,11 @@ public class LevelLoader : MonoBehaviour
             player.position = new Vector2(-31.37f, -48.16f);
             Camera.main.transform.position = new Vector3(0f, 0f, -10f); 
         }
+        if (levelIndex == 2)
+        {
+            player.position = new Vector2(0f, 0f);
+            Camera.main.transform.position = new Vector3(0f, 0f, -10f);
+        }
 
     }
 
