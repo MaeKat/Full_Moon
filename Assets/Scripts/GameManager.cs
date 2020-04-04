a09c570095a84ffd5c3f3778a3ac2a68f4fa80fc
Rachel <rachelepitcher@gmail.com>
2020-04-04 02:31:53 +0000

    Main & Pause

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void Menu() {
+        SceneManager.LoadScene("MainMenu");
+    }  
+    
+    //add a restart button
+    public void Restart() {
+        SceneManager.LoadScene("Hometown");
+    }
+}
