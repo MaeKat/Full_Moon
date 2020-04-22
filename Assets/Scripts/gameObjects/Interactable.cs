73d853a70c324c20b5a7e1f18b090bdda0703cff
Vanessa Hoffmann <hoffmann.vanessa13@gmail.com>
2020-04-22 15:46:07 +0000

    added scripts interactable

---
+++ Assets/Scripts/gameObjects/Interactable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    
+
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+private void OnTriggerEnter2D(Collider2D other){
+    if(other.CompareTag("Player") && !other.isTrigger){
+        context.Raise();
+        playerInRange = true;
+    }
+}
+
+private void OnTriggerExit2D(Collider2D other){
+    if(other.CompareTag("Player") && !other.isTrigger){
+        context.Raise();
+        playerInRange = false;
+    }
+}
+
+}
