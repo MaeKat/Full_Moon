cdc76ecc70c6a18d1f49ea6df371ce65180ac0b9
Brian Gates <briangates130@gmail.com>
2020-04-28 01:22:17 +0000

    Upload initial sound files

--- Assets/Scripts/gameObjects/Interactable.cs
+++ Assets/Scripts/gameObjects/Interactable.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     
+
     public bool playerInRange;
 
     // Start is called before the first frame update
@@ -15,12 +16,14 @@ public class Interactable : MonoBehaviour
 
 private void OnTriggerEnter2D(Collider2D other){
     if(other.CompareTag("Player") && !other.isTrigger){
+        context.Raise();
         playerInRange = true;
     }
 }
 
 private void OnTriggerExit2D(Collider2D other){
     if(other.CompareTag("Player") && !other.isTrigger){
+        context.Raise();
         playerInRange = false;
     }
 }
