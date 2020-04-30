39f7ce8b9d94a3b992706a929bafff868728d781
Vanessa Hoffmann <hoffmann.vanessa13@gmail.com>
2020-04-30 19:13:19 +0000

    -create script for mtn key and gate -added alfie moving to prefab -

---
+++ Assets/Scripts/TransitionsScripts/MtnGate/MtnGate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && MtnKey.HasKey >= 2)
+        { //Add Door collision here!
+            MtnKey.HasKey = 0;
+            Debug.Log("doorhit");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TransitionsScripts/MtnGate/MtnKey.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MtnKey : MonoBehaviour
+{
+      public static int HasKey;
+
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        { 
+            Debug.Log("gotKey");
+            HasKey +=2;
+            Destroy(gameObject);
+        }
+    }
+}
