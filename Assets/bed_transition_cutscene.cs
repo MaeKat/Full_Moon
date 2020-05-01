8fdef58a403907ba3b4875e8e626a4f36fc6538f
Victoria Chan <vicharuchan@gmail.com>
2020-05-01 23:55:24 +0000

    No Comments

---
+++ Assets/bed_transition_cutscene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bed_transition_cutscene: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine (JumpToScene());
+    }
+
+    IEnumerator JumpToScene() {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene("Cutscene 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
