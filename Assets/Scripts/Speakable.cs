708ce8ae4950e37f2ee2d29257418fafda893b25
Brian Gates <briangates130@gmail.com>
2020-05-01 18:46:02 +0000

    Added Speaker script, dialogue in Hometown, Forest

---
+++ Assets/Scripts/Speakable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speakable : MonoBehaviour
+{
+    public Text theText;
+
+    public TextAsset textfile;
+    public String[] dialogue;
+
+    public int currLine;
+    public int endAtLine;
+
+    public PlayerMovement player;
+
+    public BoxCollider2D speakerBox;
+    public BoxCollider2D alfieBox;
+    public Boolean isTouching = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+
+        if (textfile != null)
+        {
+            dialogue = (textfile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = dialogue.Length - 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theText.text = dialogue[currLine];
+
+        if (alfieBox.IsTouching(speakerBox)) {
+            isTouching = true;
+        }
+
+        if (alfieBox.IsTouching(speakerBox) && (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space)))
+        {
+            currLine += 1;
+        }
+
+        if (currLine >= endAtLine)
+        {
+            currLine = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/TextImporter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextImporter : MonoBehaviour
+{
+    public Text theText;
+
+    public TextAsset textfile;
+    public String[] dialogue;
+
+    public int currLine;
+    public int endAtLine;
+
+    public PlayerMovement player;
+
+    public Boolean onlySpeakOnce;
+    public Boolean saidOnce = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+
+        if (textfile != null)
+        {
+            dialogue = (textfile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = dialogue.Length-1;
+        }
+    }
+
+    void Update()
+    {
+        if (onlySpeakOnce && saidOnce)
+        {
+            theText.text = "";
+        }
+        else
+        {
+            theText.text = dialogue[currLine];
+
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+            {
+                currLine += 1;
+            }
+
+            if (currLine > endAtLine)
+            {
+                currLine = 0;
+                saidOnce = true;
+            }
+        }
+
+        
+
+    }
+}
