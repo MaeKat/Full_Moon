7950abb3e42c35c562f24212454e03026dd88fce
Victoria Chan <vicharuchan@gmail.com>
2020-05-01 00:50:15 +0000

    No Comments

---
+++ Assets/cutscene_end.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cutscene_end : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine (JumpToScene());
+    }
+
+    IEnumerator JumpToScene() {
+        yield return new WaitForSeconds(15.0f);
+        SceneManager.LoadScene("Alfie_Room_wolf");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
